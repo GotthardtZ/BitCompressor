8fc6c816c17ad9de7538b03dcbe3a0ebafcef8ef
Zoltan.Gotthardt <Zoltan.Gotthardt@ovivowater.com>
2022-01-24 21:50:20 +0100

    Add project files.

---
+++ BitCompressor/ArithmeticCoder.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.IO;
+
+namespace ArthmeticCoder
+{
+    class Encoder
+    {
+        private uint x1 = 0;
+        private uint x2 = uint.MaxValue; // Range, initially [0, 1), scaled by 2^32
+        private uint pending_bits = 0;
+        private uint B = 0; // a byte from the archive
+        private uint bits_in_B = 0;
+        private BinaryWriter _bw;
+
+        public Encoder(BinaryWriter bw)
+        {
+            _bw = bw;
+        }
+
+        // write the archive bit by bit
+        private void bit_write(uint bit)
+        {
+            B = (B << 1) + bit;
+            bits_in_B++;
+            if (bits_in_B == 8)
+            {
+                _bw.Write((byte)B);
+                B = 0;
+                bits_in_B = 0;
+            }
+        }
+
+        private void bit_write_with_pending(uint bit)
+        {
+            bit_write(bit);
+            for (; pending_bits > 0; pending_bits--)
+                bit_write(bit ^ 1);
+        }
+
+        private void bit_flush()
+        {
+            do
+            {
+                bit_write_with_pending(x1 >> 31); // pad pending byte from x1
+                x1 <<= 1;
+            } while (bits_in_B != 0);
+        }
+
+        public void Encode(uint bit, uint p) //p: 32 bit
+        {
+            uint xmid = x1 + (uint)(((ulong)(x2 - x1) * p) >> 32);
+            if (!(xmid >= x1 && xmid < x2)) throw new Exception();
+            if (bit != 0) x2 = xmid; else x1 = xmid + 1;
+            while (((x1 ^ x2) >> 31) == 0)
+            {  // pass equal leading bits of range
+                bit_write_with_pending(x2 >> 31);
+                x1 <<= 1;
+                x2 = (x2 << 1) | 1;
+            }
+            while (x1 >= 0x40000000 && x2 < 0xC0000000)
+            {
+                pending_bits++;
+                x1 = (x1 << 1) & 0x7FFFFFFF;
+                x2 = (x2 << 1) | 0x80000001;
+            }
+        }
+        public void Flush()
+        {
+            bit_flush();
+        }
+    }
+
+    class Decoder
+    {
+        private uint x1 = 0;
+        private uint x2 = uint.MaxValue; // Range, initially [0, 1), scaled by 2^32
+        private uint x = 0;
+        private uint B = 0; // a byte from the archive
+        private uint bits_in_B = 0;
+        private BinaryReader _br;
+
+        public Decoder(BinaryReader br)
+        {
+            _br = br;
+            for (int i = 0; i < 32; i++)
+                x = (x << 1) | bit_read();
+        }
+
+        // read the archive bit by bit
+        uint bit_read()
+        {
+            if (bits_in_B == 0)
+            {
+                if (_br.BaseStream.Position != _br.BaseStream.Length)
+                    B = _br.ReadByte();
+                else
+                    B = 255; // EOF
+                bits_in_B = 8;
+            }
+            bits_in_B--; //7..0
+            return (B >> (int)bits_in_B) & 1;
+        }
+
+        // Decompress and return next bit 
+        public uint Decode(uint p) //p: 32 bit
+        {
+            uint xmid = x1 + (uint)(((ulong)(x2 - x1) * p) >> 32);
+            if (!(xmid >= x1 && xmid < x2)) throw new Exception();
+            uint bit = x <= xmid ? 1u : 0u;
+            if (bit != 0) x2 = xmid; else x1 = xmid + 1;
+            while (((x1 ^ x2) >> 31) == 0)
+            {  // pass equal leading bits of range
+                x1 <<= 1;
+                x2 = (x2 << 1) + 1;
+                x = (x << 1) + bit_read();
+            }
+            while (x1 >= 0x40000000 && x2 < 0xC0000000)
+            {
+                x1 = (x1 << 1) & 0x7FFFFFFF;
+                x2 = (x2 << 1) | 0x80000001;
+                x = (x << 1) ^ 0x80000000;
+                x += bit_read();
+            }
+            return bit;
+        }
+    }
+}
---
+++ BitCompressor/Program.cs
@@ -0,0 +1,128 @@
+﻿using ArthmeticCoder;
+using System;
+using System.IO;
+
+namespace BitCompressor
+{
+    class Program
+    {
+        static void HowToUse()
+        {
+            Console.WriteLine("Encodes a file bit by bit using an arithmetic encoder with a static bit probability");
+            Console.WriteLine();
+            Console.WriteLine("Usage: BitCompressor.exe [e|d] input output");
+            Console.WriteLine(" - e: to endode");
+            Console.WriteLine(" - d: to dedode");
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                HowToUse();
+                return;
+            }
+            string command = args[0];
+            string inputFilename = args[1];
+            string outputFilename = args[2];
+            try
+            {
+                if (command == "e")
+                {
+                    byte[] input = File.ReadAllBytes(inputFilename);
+                    byte[] output = Encode(input);
+                    File.WriteAllBytes(outputFilename, output);
+                    Console.WriteLine($"{inputFilename}\t{input.Length}\t{output.Length}");
+                    Console.WriteLine();
+                }
+                else if (command == "d")
+                {
+                    byte[] input = File.ReadAllBytes(inputFilename);
+                    byte[] output = Decode(input);
+                    File.WriteAllBytes(outputFilename, output);
+                    Console.WriteLine($"{inputFilename}\t{input.Length}\t{output.Length}");
+                    Console.WriteLine();
+                }
+                else
+                {
+                    HowToUse();
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        static double GetBitProbability(byte[] bytes)
+        {
+            ulong n0 = 0;
+            ulong n1 = 0;
+            for (uint i = 0; i < bytes.Length; i++)
+            {
+                for (int j = 7; j >= 0; j--)
+                {
+                    uint bit = (uint)(bytes[i] >> j) & 1;
+                    n0 += bit == 0 ? 1u : 0u;
+                    n1 += bit == 1 ? 1u : 0u;
+                }
+            }
+            return (n1 + 0.5d) / (n0 + n1 + 1.0d);
+        }
+
+        const double M = uint.MaxValue + 1.0d; // 2^32
+        static byte[] Encode(byte[] input)
+        {
+            double bitProbability = GetBitProbability(input);
+            Console.WriteLine("Probability of bit=1 is " + bitProbability);
+            using MemoryStream ms = new MemoryStream();
+            using BinaryWriter bw = new BinaryWriter(ms);
+            Encoder encoder = new Encoder(bw);
+            uint filesize = (uint)input.Length;
+            bw.Write(filesize);
+
+            uint p = (uint)(bitProbability * M);
+            if (p == 0) p = 1;
+            bw.Write(p);
+
+            for (int i = 0; i < filesize; i++)
+            {
+                byte b = input[i];
+                for (int j = 7; j >= 0; j--)
+                {
+                    uint bit = (uint)(b >> j) & 1;
+                    encoder.Encode(bit, p);
+                }
+            }
+            encoder.Flush();
+            ms.Flush();
+            return ms.ToArray();
+        }
+
+        static byte[] Decode(byte[] input)
+        {
+            using MemoryStream reader = new MemoryStream(input);
+            using MemoryStream writer = new MemoryStream();
+            using BinaryReader br = new BinaryReader(reader);
+            uint origFileSize = br.ReadUInt32();
+            uint p = br.ReadUInt32();
+            Console.WriteLine("Probability of bit=1 is " + (p/M));
+            Decoder decoder = new Decoder(br);
+
+            byte b = 0;
+            for (int i = 0; i < origFileSize; i++)
+            {
+                for (int j = 7; j >= 0; j--)
+                {
+                    uint bit = decoder.Decode(p);
+                    b = (byte)((b << 1) | bit);
+                }
+                writer.WriteByte(b);
+            }
+            return writer.ToArray();
+        }
+
+    }
+}
